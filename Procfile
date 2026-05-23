4dc22b51e9efd2a61009eacb8eec51d4f7002fc6
박윤희 <bag-yunhui@bag-yunhuiui-MacBookAir.local>
2026-05-24 06:11:26 +0900

    Static 파일 설정

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn config.wsgi --bind 0.0.0.0:$PORT
+web: python manage.py collectstatic --noinput && gunicorn 
+config.wsgi --bind 0.0.0.0:$PORT
--- config/settings.py
+++ config/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,3 +134,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CSRF_TRUSTED_ORIGINS = ['https://django-wedding-platform-production.up.railway.app']
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
